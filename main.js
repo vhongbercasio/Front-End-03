7af259fe9d42e22c20ba26b55f6dda71cbcd619b
@bercasiobong <bercasiobongbong@gmail.com>
2022-09-25 14:51:02 +0800

    added deliverable

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 // change navbar style on scroll to
 
-
 window.addEventListener('scroll', () => {
     document.querySelector("nav").classList.toggle('window-scroll', window.scrollY > 0);
 
+});
+
+// Show/hide faq answer 
+
+const faqsEL = document.querySelectorAll(".faq");
+
+faqsEL.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("open");
+
+        // Change icon 
+        const icon = faq.querySelector(".faq_icon i");
+        if (icon.className === "fa-solid fa-plus") {
+            icon.className = "fa-solid fa-minus";
+
+        } else {
+            icon.className = "fa-solid fa-plus";
+        }
+    })
 })
